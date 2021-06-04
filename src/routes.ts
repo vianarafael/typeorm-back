e44b158e7502c42b2e7eac1a5e06127ce625cddc
vianarafael <vianarafaelds@gmail.com>
2021-06-04 10:52:51 +0900

    ready to go

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,11 @@
-import { Router, Request, Response } from 'express';
-import { getTasks } from './controller/TasksController';
+import { Router, Request, Response } from "express";
+import { getTasks } from "./controller/TasksController";
 const routes = Router();
 
+routes.get("/", (request: Request, response: Response) => {
+  return response.json({ message: "What's up CC20/J5?" });
+});
 
-routes.get('/', (request: Request, response: Response) => {
-    return response.json({ message: "What's up CC18?" })
-})
-
-routes.get('/tasks', getTasks);
+routes.get("/tasks", getTasks);
 
 export default routes;
